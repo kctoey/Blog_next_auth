c47559b65f0ca0a2d0db65c51164deb9f40193bc
kctoey <toeyknchprn@gmail.com>
2023-07-28 11:39:50 +0700

    fix

--- src/app/blog/blog.js
+++ src/app/blog/blog.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 import moment from "moment";
 async function getData() {
   try {
-    const res = await fetch("http://localhost:3000/api/posts", {
+    const res = await fetch("http://127.0.0.1:3000/api/posts", {
       cache: "no-store",
     });
 
