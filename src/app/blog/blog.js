2265fd6b227d04294c13b7a5428599b1ac854eb9
kctoey <toeyknchprn@gmail.com>
2023-07-28 10:01:31 +0700

    update

--- src/app/blog/blog.js
+++ src/app/blog/blog.js
@@ -2,6 +2,20 @@ import React from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
 import moment from "moment";
+export const getStaticProps = async () => {
+  const res = await fetch(process.env.NEXTAUTH_URL + "api/post2", {
+    cache: "no-store",
+  });
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const data = await res.json();
+  return {
+    props: {
+      data,
+    },
+  };
+};
 
 const Blog = ({ data }) => {
   return (
--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -4,25 +4,11 @@ export const metadata = {
   title: "Blog",
   description: "Blog Content",
 };
-
-export const getStaticProps = async () => {
-  const res = await fetch(process.env.NEXTAUTH_URL + "api/post2", {
-    cache: "no-store",
-  });
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-  const data = await res.json();
-  return {
-    props: {
-      data,
-    },
-  };
-};
-export default function Page({ data }) {
+const Page = () => {
   return (
     <div>
-      <Blog data={data} />
+      <Blog />
     </div>
   );
-}
+};
+export default Page;
