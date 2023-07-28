d588455c70123f58e7361f0f392778475577a1f8
kctoey <toeyknchprn@gmail.com>
2023-07-28 10:31:45 +0700

    update blog

--- src/app/blog/blog.js
+++ src/app/blog/blog.js
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 import Image from "next/image";
 import moment from "moment";
 async function getData() {
-  const res = await fetch("http://localhost:3000/api/posts", {
+  const res = await fetch(process.env.NEXTAUTH_URL + "/api/posts", {
     cache: "no-store",
   });
 
