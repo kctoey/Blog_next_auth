9de0c098e20d4e9de8eb762b282b164e7ad678c8
kctoey <toeyknchprn@gmail.com>
2023-07-29 14:46:56 +0700

    update blog

--- src/app/blog/[id]/page.js
+++ src/app/blog/[id]/page.js
@@ -7,7 +7,7 @@ async function getData(id) {
   const res = await fetch(`http://127.0.0.1:3000/api/posts/${id}`, {
     cache: "no-store",
   });
-
+  console.log(id);
   if (!res.ok) {
     return notFound();
   }
